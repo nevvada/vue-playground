6c085a33c6a2f83159a833a511783ec79edd84b5
nlemast <nlemaster47@gmail.com>
2021-04-25 17:26:19 -0400

    Add methods to invoke on hover and button click of colors and buttons

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -6,27 +6,43 @@ const app = new Vue({
     product: 'Socks',
     targetty: '_blank',
     inventory: 11,
-    inStock: false,
     onSale: true,
     details: [
       '80% cotton',
       '20% polyester',
       'Gender-neutral',
     ],
+    inStock: true,
     variants: [
       {
         id: 2234,
         color: 'green',
+        image: './assets/greensocks.jpeg',
       },
       {
         id: 2235,
         color: 'blue',
+        image: './assets/bluesocks.jpeg',
       }
     ],
     sizes: [
       'S',
       'M',
       'L',
-    ]
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateProduct(image) {
+      this.image = image;
+    }
   },
 });
