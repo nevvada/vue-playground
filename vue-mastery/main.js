2b65b0f13e7dc05c3b53dd4df6c03b5882fa63f8
nlemast <nlemaster47@gmail.com>
2021-04-26 15:59:45 -0400

    Attempt some event bus stuff, creating tabs

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -1,3 +1,7 @@
+const eventBus = new Vue({
+
+})
+
 Vue.component('product-details', {
   props: {
     details: {
@@ -19,7 +23,7 @@ Vue.component('product', {
   },
   methods: {
     addToCart(id) {
-      this.$emit('add-to-cart', id);
+      eventBus.$emit('add-to-cart', id);
     },
     removeFromCart() {
       this.$emit('remove-from-cart');
@@ -62,6 +66,7 @@ Vue.component('product', {
         '20% polyester',
         'Gender-neutral',
       ],
+      reviews: [],
       variants: [
         {
           id: 2234,
@@ -89,7 +94,7 @@ Vue.component('product', {
   <div class="product-image">
     <img v-bind:src="image" alt="">
   </div>
-  
+
   <div class="product-info">
     <h1>{{ title }}</h1>
     <p
@@ -130,7 +135,7 @@ Vue.component('product', {
 
     <button @click="removeFromCart()">Remove from Cart</button>
 
-    <p>{{ printBrandAndProduct }}</p>
+    <product-tabs :reviews="reviews"></product-tabs>
   </div>
 
   <a :href="hreffy" :target="targetty">
@@ -138,8 +143,143 @@ Vue.component('product', {
   </a>
 
   </div>
-  `
+  `,
+  mounted() {
+    eventBus.$on('review-submitted', productReview => {
+      this.reviews.push(productReview)
+    })
+  },
 });
+
+Vue.component('product-tabs', {
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    }
+  },
+  template: `
+  <div>
+    <div>
+      <span
+        class="tab"
+        v-for="(tab, index) in tabs"
+        :class="tab === selectedTab && 'activeTab'"
+        @click="selectedTab = tab"
+        :key="index"
+      >
+        {{ tab }}
+      </span>
+    </div>
+
+  <div v-show="selectedTab === 'Reviews'">
+  <p v-if="!reviews.length">There are no reviews yet</p>
+  <ul v-else>
+    <li v-for="review in reviews">
+      <p>{{ review.name }}</p>
+      <p>Review: {{ review.review }}</p>
+      <p>Rating: {{ review.rating }}</p>
+      <p>Recommend to a friend?: {{ review.recommend }}</p>
+    </li>
+  </ul>
+  </div>
+
+  <product-review v-show="selectedTab === 'Add a Review'"></product-review>
+
+  </div>
+
+  `,
+  data() {
+    return {
+      tabs: ['Reviews', 'Add a Review'],
+      selectedTab: 'Reviews'
+    }
+  }
+})
+
+Vue.component('product-review', {
+  template: `
+    <form @submit.prevent="submitReview">
+
+      <ul>
+        <li v-if="errors.length" v-for="error in errors">{{ error }}</li>
+      </ul>
+
+      <p>
+        <label for="name">Name: </label>
+        <input type="text" v-model="name">
+      </p>
+
+      <p>
+        <label for="review">Review: </label>
+        <textarea id="review" v-model="review" />
+      </p>
+
+      <p>
+        <label for="rating">Rating: </label>
+        <select id="rating" name="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <p>Would you recommend to a frend?</p>
+        <label for="yes">Yes</label>
+        <input type="radio" value="yes" id="yes" name="recommend" v-model="recommend">
+
+        <label for="no">No</label>
+        <input type="radio" value="no" id="no" name="recommend" v-model="recommend">
+      </p>
+
+      <input type="submit" value="Submit Review">
+
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      recommend: null,
+      errors: [],
+    }
+  },
+  methods: {
+    submitReview() {
+      this.resetErrors();
+
+      if (this.review && this.rating && this.name && this.recommend) {
+        const formattedReview = {
+          review: this.review,
+          rating: this.rating,
+          name: this.name,
+          recommend: this.recommend,
+        };
+
+        this.reviews.push(formattedReview);
+        this.resetReview();
+      } else {
+        if (!this.review) this.errors.push('Missing Review');
+        if (!this.rating) this.errors.push('Missing Rating');
+        if (!this.name) this.errors.push('Missing Name');
+        if (!this.recommend) this.errors.push('Missing Recommendation');
+      }
+    },
+    resetReview() {
+      this.name = null;
+      this.rating = null;
+      this.review = null;
+      this.recommend = null;
+    },
+    resetErrors() {
+      this.errors = [];
+    }
+  }
+})
 
 const app = new Vue({
   el: '#app',
